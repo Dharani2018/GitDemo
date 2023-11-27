4a3ec991a54ba1dfc5b81879a53ef60a7f735966
Dharani2018 <dharaniparimella13@gmail.com>
2023-11-27 14:04:43 +0530

    TL.

--- DemoProj/src/com/dp/beans/Util.java
+++ DemoProj/src/com/dp/beans/Util.java
@@ -7,7 +7,7 @@ public class Util {
 	}
 	
 	public void sub() {
-		System.out.println("sub method");
+		System.out.println("sub method.......");
 	}
 	public void m1() {
 		System.out.println("Util.m1():.........abcd.....");
